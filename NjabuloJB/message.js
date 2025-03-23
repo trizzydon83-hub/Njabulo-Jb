3bc6b66f7e1b5d47d6bbfebb19b2a0f7a49d2a96
NjabuloJ <fanajb65@gmail.com>
2025-03-23 21:39:40 +0200

    MESSAGE VIA JB

---
+++ NjabuloJB/message.js
@@ -0,0 +1,6 @@
+WELCOME TO NJABULO JBHELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:
+
+----------------
+• ᚛Njabulo᚜ : https://wa.me/26777821911
+----------------
+• Marisel : https://wa.me/254740007567
