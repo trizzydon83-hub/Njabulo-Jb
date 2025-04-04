a62555307fe33be2bc623080ed7a131a32008cdd
NjabuloJ <fanajb65@gmail.com>
2025-04-04 02:23:04 +0200

    Delete media directory

--- media/message.js
+++
@@ -1,6 +0,0 @@
-WELCOME TO NJABULO JBHELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:
-
-----------------
-• ᚛Njabulo᚜ : https://wa.me/26777821911
-----------------
-• Marisel : https://wa.me/254740007567
